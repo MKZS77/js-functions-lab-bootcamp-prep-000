b064ced4234a1f9197f34269fa3f4c246df1b6a0
MKZ <mksau77@gmail.com>
2018-07-08 15:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`;
 }
 
-var holiday = "Independence Day";
-var name = "you";
-
 function happyHolidayTo (holiday, name) {
  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown (holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
 }
